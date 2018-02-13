164af3c17c827449dd882b8387b372db4339d310
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-02-13 15:22:55 +0100

    Added jQuery and deleting/creating products option for admin

--- MilitaryStore/Models/EFProductRepository.cs
+++ MilitaryStore/Models/EFProductRepository.cs
@@ -33,5 +33,16 @@ namespace MilitaryStore.Models
             }
             context.SaveChanges();
         }
+
+        public Product DeleteProduct(int productID)
+        {
+            Product dbEntry = context.Products.FirstOrDefault(p => p.ProductID == productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
     }
 }
--- MilitaryStore/Models/IProductRepository.cs
+++ MilitaryStore/Models/IProductRepository.cs
@@ -9,5 +9,6 @@ namespace MilitaryStore.Models
     {
         IEnumerable<Product> Products { get; }
         void SaveProduct(Product product);
+        Product DeleteProduct(int productID);
     }
 }
