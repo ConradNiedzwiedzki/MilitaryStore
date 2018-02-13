8f5ca41402baf074f726ac48542525f3b3ebc979
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-02-13 13:26:54 +0100

    Added checkout views

---
+++ MilitaryStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilitaryStore.Models;
+
+namespace MilitaryStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
