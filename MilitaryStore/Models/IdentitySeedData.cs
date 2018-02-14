549ee65e86caf77b61d0a5c9f2e57f06f6cf7c84
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-02-14 14:55:17 +0100

    Small changes

--- MilitaryStore/Models/IdentitySeedData.cs
+++ MilitaryStore/Models/IdentitySeedData.cs
@@ -7,15 +7,15 @@ namespace MilitaryStore.Models
 {
     public static class IdentitySeedData
     {
-        private const string adminUser = "admin";
-        private const string adminPassword = "Hax123";
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
         public static async void EnsurePopulated(IApplicationBuilder app)
         {
             UserManager<IdentityUser> userManager = app.ApplicationServices.GetRequiredService<UserManager<IdentityUser>>();
             IdentityUser user = await userManager.FindByIdAsync(adminUser);
             if (user == null)
             {
-                user = new IdentityUser("admin");
+                user = new IdentityUser("Admin");
                 await userManager.CreateAsync(user, adminPassword);
             }
         }
