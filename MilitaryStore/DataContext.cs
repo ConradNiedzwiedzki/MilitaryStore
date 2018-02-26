f9814c76a4185c1b221a314e4b9ab30bdf2b7ff9
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-02-26 14:49:05 +0100

    Fixed IDesignTimeDbContextFactory problem.

---
+++ MilitaryStore/DataContext.cs
@@ -0,0 +1,6 @@
+﻿namespace MilitaryStore
+{
+    public class DataContext
+    {
+    }
+}
---
+++ MilitaryStore/Models/DesignTimeDbContextFactoryIdentity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilitaryStore.Models
+{
+    public class DesignTimeDbContextFactoryIdentity : IDesignTimeDbContextFactory<AppIdentityDbContext>
+    {
+        public AppIdentityDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<AppIdentityDbContext>();
+            var connectionString = configuration["Data:MilitaryStoreIdentity:ConnectionString"];
+            builder.UseSqlServer(connectionString);
+            return new AppIdentityDbContext(builder.Options);
+        }
+    }
+}
